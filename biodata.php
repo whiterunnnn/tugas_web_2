73c559a03f38acfce64229e7ec72fe3159a92ff5
n3wbie <135624098+whiterunnnn@users.noreply.github.com>
2023-06-14 19:50:07 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,17 @@
+<?php
+
+$nama="Ganendra Riosetya Nugrahanto";
+$tempatlahir="Semarang";
+$tanggallahir="12-05-2003";
+$hobi="Main Game, Scroll Tiktok";
+
+echo "Nama Saya : $nama<br>";
+echo "Tempat Lahir : $tempatlahir<br>";
+echo "Tanggal Lahir : $tanggallahir<br>";
+echo "Hobi : $hobi<br>";
+
+
+
+
+
+?>
